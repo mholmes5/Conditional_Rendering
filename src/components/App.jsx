cd87b47a7f7a56bd310b8f7a2a7c9c5bd992f799
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 16:59:57 -0500

    add Ternary and && operators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = false;
+var isLoggedIn = true;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+const currentTime = new Date().getHours();
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {
+        //isLoggedIn ? <h1>Hello</h1> : <Login />
+        currentTime > 15 && <h1>Why are you still working?</h1>
+      }
+    </div>
+  );
 }
 
 export default App;
