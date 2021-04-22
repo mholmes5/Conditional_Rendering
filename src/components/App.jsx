588d75f7f304a8a0ceb65da3b13fa819687d7cbe
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 16:32:41 -0500

    create renderConditionally function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn) {
+    return <h1>Hello</h1>;
+  } else {
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
-    </div>
-  );
+    );
+  }
+}
+
+function App() {
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
